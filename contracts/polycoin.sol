a3d0d169387895a388d53bc653b5ff4fa7b21c61
RyanSmoak <coleearth13@gmail.com>
2024-05-08 15:31:11 +0300

    add polycoin smart contract

---
+++ contracts/polycoin.sol
@@ -0,0 +1,114 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+interface ERC20Interface {
+    function totalSupply() external view returns (uint256);
+
+    function balanceOf(address tokenOwner)
+        external
+        view
+        returns (uint256 balance);
+
+    function allowance(address tokenOwner, address spender)
+        external
+        view
+        returns (uint256 remaining);
+
+    function transfer(address to, uint256 tokens)
+        external
+        returns (bool success);
+
+    function approve(address spender, uint256 tokens)
+        external
+        returns (bool success);
+
+    function transferFrom(
+        address from,
+        address to,
+        uint256 tokens
+    ) external returns (bool success);
+
+    event Transfer(address indexed from, address indexed to, uint256 tokens);
+    event Approval(
+        address indexed tokenOwner,
+        address indexed spender,
+        uint256 tokens
+    );
+}
+
+//Actual token contract
+
+contract PTKToken is ERC20Interface {
+    string public symbol;
+    string public name;
+    address owner;
+    uint8 public decimals;
+    uint256 public _totalSupply;
+
+    mapping(address => uint256) balances;
+    mapping(address => mapping(address => uint256)) allowed;
+
+    constructor(string memory _name, string memory _symbol) {
+        name = _name;
+        symbol = _symbol;
+        _totalSupply = 1000000;
+        owner = msg.sender;
+        balances[owner] = _totalSupply;
+        emit Transfer(
+            address(0),
+            owner,
+            _totalSupply
+        );
+    }
+
+    function totalSupply() public view returns (uint256) {
+        return _totalSupply - balances[address(0)];
+    }
+
+    function balanceOf(address tokenOwner)
+        public
+        view
+        returns (uint256 balance)
+    {
+        return balances[tokenOwner];
+    }
+
+    function transfer(address to, uint256 tokens)
+        public
+        returns (bool success)
+    {
+        balances[msg.sender] -= tokens;
+        balances[to] += tokens;
+        emit Transfer(msg.sender, to, tokens);
+        return true;
+    }
+
+    function approve(address spender, uint256 tokens)
+        public
+        returns (bool success)
+    {
+        allowed[msg.sender][spender] = tokens;
+        emit Approval(msg.sender, spender, tokens);
+        return true;
+    }
+
+    function transferFrom(
+        address from,
+        address to,
+        uint256 tokens
+    ) public returns (bool success) {
+        balances[msg.sender] -= tokens;
+        allowed[from][msg.sender] -= tokens;
+        balances[to] += tokens;
+        emit Transfer(from, to, tokens);
+        return true;
+    }
+
+    function allowance(address tokenOwner, address spender)
+        public
+        view
+        returns (uint256 remaining)
+    {
+        return allowed[tokenOwner][spender];
+    }
+}
